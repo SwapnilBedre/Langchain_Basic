192a1dfaec42b93b0addbcbeaefdfa625d11867a
SwapnilBedre <swapnilbedre204@gmail.com>
2025-08-16 02:55:17 +0530

    Modified

--- 4_RAG/1a_basic_part_1.py
+++ 4_RAG/1a_basic_part_1.py
@@ -3,13 +3,16 @@ from langchain_groq import ChatGroq
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_community.document_loaders import TextLoader
 from langchain_community.vectorstores import Chroma
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.document_loaders import TextLoader
+
 
 
 # defining the derectory containing the text files and the persistant directory
 current_dir = os.path.dirname(os.path.abspath(__file__))
-file_path = os.path.join(current_dir, "documents", "hulk_description.txt")
+file_path = os.path.join(current_dir, "documents", "batman_description.txt")
 persistant_directory = os.path.join(current_dir, "db", "chroma_db")
+
 
 # Use croma DB to store vectors.
 if not os.path.exists(persistant_directory):
@@ -22,7 +25,8 @@ if not os.path.exists(persistant_directory):
         )
     
     # reload the text content from the file
-    loader = TextLoader(file_path)
+    # loader = TextLoader("f:/Langchain_Basic/4_RAG/documents/batman_description.txt", encoding="utf-8")
+    loader = TextLoader(file_path, encoding="utf-8")
     documents = loader.load()
 
     # split the text into chunks
