62c8b9de5f29b64dd5f888eac09c4f0cad514b10
SwapnilBedre <swapnilbedre204@gmail.com>
2025-08-16 13:02:15 +0530

    A

--- 4_RAG/1a_basic_part_1.py
+++ 4_RAG/1a_basic_part_1.py
@@ -3,19 +3,19 @@ from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_groq import ChatGroq
 import os
-from langchain_community.vectorstores import Chroma
+from langchain_chroma import Chroma
 from langchain_community.document_loaders import TextLoader
 
 
 
-# defining the derectory containing the text files and the persistant directory
+# defining the directory containing the text files and the persistant directory
 current_dir = os.path.dirname(os.path.abspath(__file__))
 file_path = os.path.join(current_dir, "documents", "batman_description.txt")
-persistant_directory = os.path.join(current_dir, "db", "chroma_db")
+persistent_directory = os.path.join(current_dir, "db", "chroma_db")
 
 
 # Use croma DB to store vectors.
-if not os.path.exists(persistant_directory):
+if not os.path.exists(persistent_directory):
     print("persistant_directory does not exist, initializing vector store...")
 
     # ensure the text file exists.
@@ -30,7 +30,7 @@ if not os.path.exists(persistant_directory):
     documents = loader.load()
 
     # split the text into chunks
-    text_splitter = CharacterTextSplitter(chunk_size=100, chunk_overlap=0)
+    text_splitter = CharacterTextSplitter(chunk_size=500, chunk_overlap=50)
     docs = text_splitter.split_documents(documents)
 
     #  Display information about the split documents
@@ -48,10 +48,15 @@ if not os.path.exists(persistant_directory):
     db = Chroma.from_documents(
         docs,
         embeddings,
-        persist_directory=persistant_directory
+        persist_directory=persistent_directory
     )
     print("Vector store created and persisted successfully.")
 
 else:
     print("Persistant directory already exists, loading vector store...")
-
+    embeddings = HuggingFaceEmbeddings(model_name="all-MiniLM-L6-v2")
+    db = Chroma(
+        persist_directory=persistent_directory,
+        embedding_function=embeddings
+    )
+    print(f"Loaded existing vector store with {db._collection.count()} documents.")
